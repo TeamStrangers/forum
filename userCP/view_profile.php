89ccad94ebe40325f94a76d8876dfccf4a0303ca
S.Luhamaa <32506616+Divoure@users.noreply.github.com>
2018-06-12 09:44:47 +0300

    Add files via upload

--- userCP/view_profile.php
+++ userCP/view_profile.php
@@ -16,38 +16,33 @@ $user = DatabaseHandler::getUserBySQLID($sqlid);
 if($user != null)
 {
 	define('CUSTOM_TITLE', $translator->getString('title', array('pagename' => $user->getUsername() . ' ' . $translator->getString('profile'))));
-	$page = '<div class="container"><img id="mainInfoLeft" src="' . $user->getAvatar() . '"><div id="mainInfoRight">';
-    $page .= '<span>' . $translator->getString('joined') . ' ' . date("F j, Y", $user->getJoindate()) . '</span>';
-    $page .= '<p>' . $user->getUsername() . '</p>';
-    $page .= '<p>' . $user->getRoleTxt() . '</p>';
-    $page .= '<p>' . htmlspecialchars($user->getMotto()) . '</p>';
-	$page .= '</div></div>';
-	$description = $user->getDescription();
-	if($user->getDescription() == '') $description = $translator->getString('emptydescription');
-	$gender = $user->getGender();
-    $page .= '<div class="container"><span class="title">' . $translator->getString('aboutme') . '</span><div class="description">' . htmlspecialchars($description) . '</span></div><span class="gender">' . $user->getGender() . '</span></div>';
-    $page .= '<div class="container"></div>';
-/*	$page = '<div class="ProfileContainer">';
+	$page = '<div class="ProfileContainer">';
 	$page .=    '<div class="Wrap">';
 	$page .=        '<section class="UpperSection">';
-	$page .=            '<div class="UpperSectionLeftStart">';
+	$page .=            '<div class="UpperSectionLeft">';
     $page .=                '<img class="avatar" src="' . $user->getAvatar() . '">';
-    $page .=            '</div><!-- UpperSectionLeftStart -->';
-    $page .=            '<div class="UpperSectionRightStart">';
-    $page .=                '<div id="NameRole"><p id="role">' . $user->getRoleTxt() . '</p><h1 id="username" >' . $user->getUsername() . '</h1></div>';
-    $page .=            '</div><!-- UpperSectionRightStart -->';
-    $page .=            '<div class="UpperSectionRightEnd">';
-    $page .=                '<div id="Status"><h4>Status</h4><p id="motto">' . $user->getMotto() . '</p></div>';
-    $page .=            '</div><!-- UpperSectionRightEnd -->';
+    $page .=            '</div><!-- UpperSectionLeft -->';
+    $page .=            '<div class="UpperSectionRight">';
+	$page .=                    '<span id="DateJoined">' . $translator->getString('joined') . ' ' . date("F j, Y", $user->getJoindate()) . '</span>';
+    $page .=                '<div class="UpperSectionRightStart">';
+    $page .=                    '<div id="NameRole"><p id="role">' . $user->getRoleTxt() . '</p><h1 id="username" >' . $user->getUsername() . '</h1></div>';
+    $page .=                '</div><!-- UpperSectionRightStart -->';
+    $page .=                '<div class="UpperSectionRightEnd">';
+    $page .=                    '<p id="motto">' . htmlspecialchars($user->getMotto()) . '</p>';
+    $page .=                '</div><!-- UpperSectionRightEnd -->';
+    $page .=            '</div><!-- UpperSectionRight-->';
     $page .=        '</section><!-- UpperSection -->';
     $page .=        '<div class="MainSection">';
     $page .=            '<p>' . $user->getNationality() . '</p>';
-    if($user->getDescription() != "No description") {
-        $page .=        '<p>' . $user->getDescription() . '</p>';
-    }
+	if($user->getDescription() == '') $description = $translator->getString('emptydescription');
+	$gender = $user->getGender();
+	if ($gender==0) $gender=$translator->getString('gender0');
+	if ($gender==1) $gender=$translator->getString('gender1');
+	if ($gender==2) $gender=$translator->getString('gender2');
+	$page .='<p>'. $gender .'</p>';
     $page .=        '</div><!-- MainSection -->';
     $page .=    '</div><!-- Wrap -->';
-    $page .='</div><!-- ProfileContainer -->';*/
+    $page .='</div><!-- ProfileContainer -->';
 }
 else
 {
